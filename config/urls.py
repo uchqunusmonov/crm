64a4e5e43b838bdbcc807b52c2fcb4f78b5cbfcd
Khasan <biismiallah40@gmail.com>
2022-04-09 19:50:50 +0500

    ..

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('dashboard.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
