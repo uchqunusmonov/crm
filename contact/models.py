952bda68df4becf972054024a97e6b79e31a7bb7
Gayrat-SE <narzullayevgayrat7@gmail.com>
2022-04-12 20:22:39 +0500

    contact_modal

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
+from accounts.models import Employe
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user = models.ForeignKey(Employe, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+    company = models.CharField(max_length=100, null=True, blank=True)
+    position = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='contact/images', null=True, blank=True)
+    
+    
