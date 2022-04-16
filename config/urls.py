cda623c278aa4ff2e55a8dd266582c7d20cc1485
Begzod <97680127+Begzod021@users.noreply.github.com>
2022-04-17 02:21:05 +0500

    update calendar

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('dashboard.urls'))
+    path('', include('dashboard.urls')),
+    path('', include('calendar_crm.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
