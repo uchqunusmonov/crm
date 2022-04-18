23cfb649af6a9202aff26f239dd393ba1daa4e87
Gayrat-SE <narzullayevgayrat7@gmail.com>
2022-04-18 16:36:09 +0500

    crm contact

--- contact/models.py
+++ contact/models.py
@@ -8,11 +8,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     user = models.ForeignKey(Employe, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=100)
-    email = models.EmailField(null=True, blank=True)
-    company = models.CharField(max_length=100, null=True, blank=True)
-    position = models.CharField(max_length=100, null=True, blank=True)
-    address = models.CharField(max_length=100, null=True, blank=True)
-    image = models.ImageField(upload_to='contact/images', null=True, blank=True)
+   
     
     
