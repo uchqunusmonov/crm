368c467affa9a869a5a6c46d887a8d135be1d732
Gayrat-SE <narzullayevgayrat7@gmail.com>
2022-05-17 18:36:58 +0500

    chat

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-
-urlpatterns = [
-    path('<str:username>', chat, name='chat'),
-    path('send-message/', send_message, name='send-message'),
-]
