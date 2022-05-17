ef8625da9774ade3181e93c53c77588c176e1e2d
Gayrat-SE <narzullayevgayrat7@gmail.com>
2022-05-17 19:12:27 +0500

    chat form deleted

--- chat/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from .models import Message
-from django import forms
-
-
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ("message", "file", "contact", "author", "group", "dialog" )
