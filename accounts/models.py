e2e314b4e9c51d0a68a37ad73228f8bba780190e
Begzod <97680127+Begzod021@users.noreply.github.com>
2022-05-20 03:26:58 +0500

    update remember_me and add token

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.conf import settings
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -26,6 +30,10 @@ class User(AbstractUser):
     def __str__(self) -> str:
         return self.username
 
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
 
 
 class Section(models.Model):
