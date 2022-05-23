4510f5c94704dfb87b603278d93ab3c295e415cb
uchqunusmonov <usmonov_uchqun@mail.ru>
2022-05-23 19:45:38 +0500

    1

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'uz'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Tashkent'
 
